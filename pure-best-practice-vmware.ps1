7e8ed3ec45223d9d9d7d67dc5ba411c4d9f5b8be
semowen <smowen@gmail.com>
2025-06-13 11:51:34 -0700

    Update pure-best-practice-vmware.ps1

--- pure-best-practice-vmware.ps1
+++ pure-best-practice-vmware.ps1
@@ -14,3 +14,26 @@ foreach ($esx in $hosts)
         $esxcli.storage.nmp.satp.rule.add.invoke($satpArgs)
  }   
   
+
+
+foreach ($esx in $hosts)
+{
+        $esxcli=get-esxcli -VMHost $esx -v2
+        $satpArgs = $esxcli.storage.nmp.satp.rule.remove.createArgs()
+        $satpArgs.description = "Pure Storage FlashArray SATP Rule"
+        $satpArgs.model = "FlashArray"
+        $satpArgs.vendor = "PURE"
+        $satpArgs.satp = "VMW_SATP_ALUA"
+        $satpArgs.psp = "VMW_PSP_RR"
+        $satpArgs.pspoption = "iops=1"
+        $esxcli.storage.nmp.satp.rule.add.invoke($satpArgs)
+}
+
+$deviceargs = $esxcli.storage.nmp.psp.roundrobin.deviceconfig.get.createargs()
+            $deviceargs.device = $device.CanonicalName
+            $deviceconfig = $esxcli.storage.nmp.psp.roundrobin.deviceconfig.get.invoke($deviceargs)
+            $nmpargs =  $esxcli.storage.nmp.psp.roundrobin.deviceconfig.set.createargs()
+            $nmpargs.iops = $iopsvalue
+            $nmpargs.type = "iops"
+
+
